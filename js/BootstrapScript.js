11d7da55dd1bde2a71d73d4f08d86b55ff8124e9
Jedikops <jedikops@gmail.com>
2016-05-21 23:25:59 +0200

    Delete BootstrapScript.js

--- js/BootstrapScript.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    $(document).ready(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-        $(".navbar-nav li").on("click", function () {
-            $(".navbar-nav li").removeClass("active");
-            $(this).addClass("active");
-        });
-    });   
-})();
